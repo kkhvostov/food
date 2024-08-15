016213569041be256690be5496f652021fc04028
kkhvostov <wusnoop86@gmail.com>
2024-08-15 21:31:44 +0300

    add php form

--- js/main.js
+++ js/main.js
@@ -207,12 +207,48 @@ window.addEventListener("DOMContentLoaded" , ()=>{
     //Forms
 
     const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(e=> {
+        postData(e);
+
+    });
+
     function postData(form){
         form.addEventListener('submit', e => {
             e.preventDefault();
 
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+
             const request = new XMLHttpRequest();
-            request.open('POST', server.php);
-        })
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object ={};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+            request.addEventListener('load', e => {
+                if (request.status === 200) {
+                    console.log(request.responseText);
+                    statusMessage.textContent = message.success;
+                }else {
+                    statusMessage.textContent = message.error;
+                }
+            })
+        });
     }
 });
